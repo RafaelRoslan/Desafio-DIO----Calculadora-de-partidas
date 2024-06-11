035e1baf7123a4caf8cae31eff2f52e71e22e61b
Rafael Roslan <rafaelroslan@gmail.com>
2024-06-11 13:07:54 -0300

    alteracoes

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const vitorias = 10;
+const vitorias = 90;
 const derrotas = 5;
 
 function saldoRankeada(vitorias, derrotas) {
     return vitorias - derrotas;
 }
-s
+
 const saldo = saldoRankeada(vitorias, derrotas);
 
 if (saldo < 10) {
-    console.log("o herói tem de saldo de " + saldo + "e está no nivel de Ferro");
+    console.log("o herói tem de saldo de " + saldo + ", e está no nivel de Ferro");
 } else if (saldo < 20) {
-    console.log("o herói tem de saldo de " + saldo + "e está no nivel de Bronze");
+    console.log("o herói tem de saldo de " + saldo + ", e está no nivel de Bronze");
 } else if (saldo < 50) {
-    console.log("o herói tem de saldo de " + saldo + "e está no nivel de Prata");
+    console.log("o herói tem de saldo de " + saldo + ", e está no nivel de Prata");
 } else if (saldo < 80) {
-    console.log("o herói tem de saldo de " + saldo + "e está no nivel de Ouro");
+    console.log("o herói tem de saldo de " + saldo + ", e está no nivel de Ouro");
  }else if (saldo < 90) {
-    console.log("o herói tem de saldo de " + saldo + "e está no nivel de Diamante");
+    console.log("o herói tem de saldo de " + saldo + ", e está no nivel de Diamante");
 }else if (saldo < 101) {
-    console.log("o herói tem de saldo de " + saldo + "e está no nivel de Lendário");
+    console.log("o herói tem de saldo de " + saldo + ", e está no nivel de Lendário");
 }else {
-    console.log("o herói tem de saldo de " + saldo + "e está no nivel de Imortal");
+    console.log("o herói tem de saldo de " + saldo + ", e está no nivel de Imortal");
 }
